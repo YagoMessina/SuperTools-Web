6730b4af3879a6bc9a5565f7e0f15b1c2757960e
YagoMessina <55106258+YagoMessina@users.noreply.github.com>
2021-12-27 14:24:51 -0300

    Started Home Design

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,25 @@
 import React from "react";
+import "./Home.css";
+import Navbar from "../navbar/Navbar";
 
 const Home = () => {
   return (
     <div>
-      <h1>Note</h1>
-      <h1>Shopping</h1>
+      <Navbar />
+      <section class="basic-grid">
+        <div class="card">1</div>
+        <div class="card">2</div>
+        <div class="card">3</div>
+        <div class="card">4</div>
+        <div class="card">5</div>
+        <div class="card">6</div>
+        <div class="card">7</div>
+        <div class="card">8</div>
+        <div class="card">9</div>
+        <div class="card">10</div>
+        <div class="card">11</div>
+        <div class="card">12</div>
+      </section>
     </div>
   );
 };
