df6fb5db029592787c6210be626a5478b0bf7ba2
YagoMessina <55106258+YagoMessina@users.noreply.github.com>
2022-01-14 20:14:44 -0300

    Build

---
+++ build/precache-manifest.2a9a56f71b50c8c6bbe86fe057e4629f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb10c6140cdc07f46f50",
+    "url": "/static/css/main.e284d378.chunk.css"
+  },
+  {
+    "revision": "bb10c6140cdc07f46f50",
+    "url": "/static/js/main.bb10c614.chunk.js"
+  },
+  {
+    "revision": "b4bef423318ac81d3631",
+    "url": "/static/js/1.b4bef423.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "12a9023bed918c343d743ce06cfac630",
+    "url": "/static/media/logo.12a9023b.png"
+  },
+  {
+    "revision": "b22d555a50454b24a199fb576dcb3c39",
+    "url": "/index.html"
+  }
+];
