abb3b7cc35b672b5ec0e693f4c15c8911277e601
YagoMessina <55106258+YagoMessina@users.noreply.github.com>
2022-01-19 22:26:46 -0300

    File input

--- src/components/file/Files.js
+++ src/components/file/Files.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import Navbar from "../navbar/Navbar";
 import File from "./File";
 
+import { doPost } from "../../util/Http";
+
 const Files = () => {
+  const file = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    doPost(
+      "/api/file",
+      {
+        file: "hola",
+      },
+      (response) => {
+        alert("peola");
+      },
+      (error) => alert(error)
+    );
+  };
+
   return (
     <div className="files">
       <Navbar />
       <main>
+        <form onSubmit={handleSubmit}>
+          <input type="file" ref={file} />
+          <input type="submit" />
+        </form>
         <table>
           <thead>
             <tr>
